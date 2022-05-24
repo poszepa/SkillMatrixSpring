8b7a3f3dc70197b4286f269e16fb207bb884cc8f
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-05-24 19:48:52 +0200

    Add Controller with action to show, remove, edit, create Team.

---
+++ src/main/java/pl/skillmatrix/skillmatrixspringboot/controller/TeamAdminController.java
@@ -0,0 +1,89 @@
+package pl.skillmatrix.skillmatrixspringboot.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pl.skillmatrix.skillmatrixspringboot.model.DepartmentsInWarehouse;
+import pl.skillmatrix.skillmatrixspringboot.model.FunctionInWarehouse;
+import pl.skillmatrix.skillmatrixspringboot.model.TeamsInWarehouse;
+import pl.skillmatrix.skillmatrixspringboot.repository.DepartmentsInWarehouseRepository;
+import pl.skillmatrix.skillmatrixspringboot.repository.FunctionInWarehouseRepository;
+import pl.skillmatrix.skillmatrixspringboot.repository.TeamsInWarehouseRepository;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@RequiredArgsConstructor
+@RequestMapping("skillMatrix/admin")
+@Controller
+public class TeamAdminController {
+    private final TeamsInWarehouseRepository teamRepository;
+    private final DepartmentsInWarehouseRepository departmentsRepository;
+
+    @GetMapping("team")
+    public String showTeam(Model model) {
+        model.addAttribute("teams",teamRepository.findAll());
+        return "/skillMatrix/admin/team";
+    }
+
+    @GetMapping("team/create")
+    public String createTeam(Model model, HttpSession httpSession) {
+        httpSession.setMaxInactiveInterval(1);
+        TeamsInWarehouse team = new TeamsInWarehouse();
+        model.addAttribute("team",team);
+        return "skillMatrix/admin/teamCreate";
+    }
+
+    @PostMapping("function/create")
+    public String createTeam(@ModelAttribute("team")TeamsInWarehouse team, HttpSession httpSession){
+        if(team == null) {
+            return "redirect:/skillMatrix/admin/team/create";
+        }
+        teamRepository.save(team);
+        httpSession.setAttribute("success", "Correctly added a new Team");
+        return "redirect:/skillMatrix/admin/team/create";
+    }
+
+    @GetMapping("function/remove/{id}")
+    public String removeTeam(@PathVariable("id")Integer id, Model model) {
+        if(!teamRepository.existsById(id)) {
+            return "redirect:/skillMatrix/admin/team";
+        }
+        model.addAttribute("team", teamRepository.findById(id));
+        return "/skillMatrix/admin/teamRemove";
+    }
+
+    @PostMapping("team/remove/remove")
+    public String removeTeam(@ModelAttribute("id")Integer id) {
+        if(!teamRepository.existsById(id)){
+            return "redirect:/skillMatrix/admin/team";
+        }
+        teamRepository.deleteById(id);
+        return "redirect:/skillMatrix/admin/team";
+    }
+
+
+
+    @GetMapping("team/edit/{id}")
+    public String modifyTeam(@PathVariable("id")Integer id, Model model) {
+        if(!teamRepository.existsById(id)){
+            return "redirect:/skillMatrix/admin/team";
+        }
+        model.addAttribute("team", teamRepository.findById(id));
+        return "/skillMatrix/admin/teamEdit";
+    }
+
+    @PostMapping("function/edit")
+    public String modifyTeam(@ModelAttribute("team") TeamsInWarehouse team) {
+        teamRepository.save(team);
+        return "redirect:/skillMatrix/admin/team";
+    }
+
+
+    @ModelAttribute("departments")
+    public List<DepartmentsInWarehouse> getAllDepartments() {
+        return departmentsRepository.findAll();
+    }
+
+}
