514678f03edffe7c2ffb508aa05e7fd235460df3
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-06-10 13:31:31 +0200

    Remove Set roles from user and change to String role

--- src/main/java/pl/skillmatrix/skillmatrixspringboot/model/User.java
+++ src/main/java/pl/skillmatrix/skillmatrixspringboot/model/User.java
@@ -1,25 +1,41 @@
 package pl.skillmatrix.skillmatrixspringboot.model;
 
 import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import java.time.LocalDateTime;
 import java.util.List;
 
 
 @Entity
 @Getter @Setter
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor @AllArgsConstructor @SuperBuilder
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
+
     @Email
     @Column(nullable = false, unique = true, length = 20)
-    private String email;
+    private String username;
+
     @Column(nullable = false)
     private String password;
-    private int enabled;
-    @ManyToOne
-    private Role role;
+
+    private boolean enabled = false;
+
+    private String role;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    private LocalDateTime createOn;
+
+    @UpdateTimestamp
+    @Column(insertable = false)
+    private LocalDateTime updateOn;
 }
