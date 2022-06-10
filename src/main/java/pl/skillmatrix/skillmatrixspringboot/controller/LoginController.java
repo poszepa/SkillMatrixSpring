05eed416427562a245dbb1cb58333f0a9a81f315
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-06-10 13:33:41 +0200

    Delete method post /login from controller

--- src/main/java/pl/skillmatrix/skillmatrixspringboot/controller/LoginController.java
+++ src/main/java/pl/skillmatrix/skillmatrixspringboot/controller/LoginController.java
@@ -3,12 +3,23 @@ package pl.skillmatrix.skillmatrixspringboot.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import pl.skillmatrix.skillmatrixspringboot.model.User;
+import pl.skillmatrix.skillmatrixspringboot.service.AuthService;
+import pl.skillmatrix.skillmatrixspringboot.service.UserService;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 
 @Controller
 @RequiredArgsConstructor
 public class LoginController {
+
+    private final UserService userService;
+    private final AuthService authService;
 
     @GetMapping("/")
     public String login(Model model){
@@ -20,5 +31,18 @@ public class LoginController {
     public String register(Model model) {
         model.addAttribute("user", new User());
         return "register";
+    }
+
+    @PostMapping("/register")
+    public String register(@ModelAttribute("user") @Valid User user , BindingResult bindingResult, HttpServletRequest req) {
+        if(bindingResult.hasErrors()) {
+            return "redirect:/register";
+        }
+        String rePassword = req.getParameter("passwordControllRe");
+        if(!user.getPassword().equals(rePassword)) {
+            return "redirect:/register";
+        }
+        userService.saveUser(user);
+        return "redirect:/";
     }
 }
