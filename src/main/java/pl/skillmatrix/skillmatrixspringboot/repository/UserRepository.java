754c7e49cdd06ae69fec3b16cf59b41ae88ff037
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-06-10 21:25:31 +0200

    Add method to find user

--- src/main/java/pl/skillmatrix/skillmatrixspringboot/repository/UserRepository.java
+++ src/main/java/pl/skillmatrix/skillmatrixspringboot/repository/UserRepository.java
@@ -1,11 +1,19 @@
 package pl.skillmatrix.skillmatrixspringboot.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import pl.skillmatrix.skillmatrixspringboot.model.User;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     Optional<User> findByUsername(String username);
+
+    @Query("SELECT user FROM User user WHERE user.role = :role")
+    List<User> findAllByRole(@Param("role")String role);
+
+    User findUserById(Integer userID);
 }
