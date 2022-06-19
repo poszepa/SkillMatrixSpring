d5dbc0bd6c7c2c1b3e860cf3407974c78cffddfa
Kamil Poszepczyński <93219293+poszepa@users.noreply.github.com>
2022-06-19 19:45:16 +0200

    Add @Builder to model

--- src/main/java/pl/skillmatrix/skillmatrixspringboot/model/GroupsInWarehouse.java
+++ src/main/java/pl/skillmatrix/skillmatrixspringboot/model/GroupsInWarehouse.java
@@ -1,7 +1,6 @@
 package pl.skillmatrix.skillmatrixspringboot.model;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import javax.validation.constraints.Max;
@@ -12,6 +11,8 @@ import java.util.List;
 @Entity
 @Getter
 @Setter
+@Builder
+@NoArgsConstructor @AllArgsConstructor
 @Table(name = "groups_in_warehouse")
 public class GroupsInWarehouse {
     @Id
